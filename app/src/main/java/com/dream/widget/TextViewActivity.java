988409e58c4ec955d95c4c982d5c22deb9870fa9
Lee <lijunbo@bandfuntech.com>
2015-09-03 23:49:47 +0800

    修改基类

--- app/src/main/java/com/dream/widget/TextViewActivity.java
+++ app/src/main/java/com/dream/widget/TextViewActivity.java
@@ -1,13 +1,11 @@
 package com.dream.widget;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
 
 import com.dream.app.R;
+import com.dream.base.BaseActivity;
 
-public class TextViewActivity extends AppCompatActivity {
+public class TextViewActivity extends BaseActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
