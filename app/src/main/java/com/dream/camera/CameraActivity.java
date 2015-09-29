3f4d87bd4c615f9615832f47abc342cc161e6e27
Lee <lijunbo@bandfuntech.com>
2015-09-29 14:14:22 +0800

    android camera use

---
+++ app/src/main/java/com/dream/camera/CameraActivity.java
@@ -0,0 +1,16 @@
+package com.dream.camera;
+
+import android.os.Bundle;
+
+import com.dream.app.R;
+import com.dream.base.BaseActivity;
+
+public class CameraActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_camera);
+    }
+
+}
