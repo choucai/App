6a0641d0be2ca16aa4349a91f04c8fefe8554bc4
william <lijunbo@yyhudong.com>
2016-05-21 09:59:06 +0800

    建立test Module，准备进行插件Demo的实战

---
+++ test/src/main/java/com/dream/test/MainActivity.java
@@ -0,0 +1,15 @@
+package com.dream.test;
+
+import android.os.Bundle;
+
+import com.dream.test.base.BaseActivity;
+
+public class MainActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
---
+++ test/src/main/java/com/dream/test/base/BaseActivity.java
@@ -0,0 +1,24 @@
+package com.dream.test.base;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * TODO.
+ *
+ * @author 李君波
+ * @version v1.0.0
+ * @created 2016-05-21 上午9:53.
+ * @phone 152-5320-8570
+ */
+public class BaseActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+    }
+
+
+}
