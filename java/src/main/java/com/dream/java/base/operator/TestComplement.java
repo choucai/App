722568ce5dceda1d7408633e0353842cd9adf455
William Lee <lijunbo@bjshanbang.com>
2016-10-21 15:07:57 +0800

    重新复习了Java的【原码】【反码】【补码】

--- java/src/main/java/com/dream/java/base/operator/TestComplement.java
+++ java/src/main/java/com/dream/java/base/operator/TestComplement.java
@@ -1,7 +1,10 @@
 package com.dream.java.base.operator;
 
 /**
- * 补码.
+ * 补码 + 反码 + 原码.
+ * http://www.ruanyifeng.com/blog/2009/08/twos_complement.html
+ *
+ * 今天又从重新知道【一般的数据类型的最高表示的是符号位】
  *
  * @author 李君波
  * @version v1.0.0
@@ -12,6 +15,8 @@ package com.dream.java.base.operator;
 public class TestComplement {
 
     public static void main(String[] args) {
-
+        System.out.println(Integer.toBinaryString(8));
+        System.out.println(Integer.toBinaryString(-8));
     }
+
 }
